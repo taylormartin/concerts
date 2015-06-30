7ea3191373454b9c33f1b8382c6885f1d82e73dc
Taylor Martin <taylormartin.uga@gmail.com>
2015-06-29 22:28:28 -0400

    basic react component on the page

---
+++ app/assets/javascripts/components/search.js.jsx
@@ -0,0 +1,9 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Concert App
+      </div>
+    );
+  }
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
 
   def index
     # hard coding "atlanta" in for now to get the data right
-    results = Songkick.city_search_by_string "atlanta"
-    results.to_json
+    #results = Songkick.city_search_by_string "atlanta"
+    #results.to_json
   end
 
 end
