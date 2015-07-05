c88b86108cbb234b084afbd2b728af97a6efd141
Taylor Martin <taylormartin.uga@gmail.com>
2015-07-05 18:04:40 -0400

    search input updating value and submit button working properly, next add ajax call

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -1,8 +1,29 @@
 var CommentBox = React.createClass({
+
+  getInitialState: function() {
+    return {
+      cityValue: "i.e. Atlanta",
+    };
+  },
+
+  handleChange: function(event) {
+    this.setState({cityValue: event.target.value});
+  },
+
+  handleClick: function() {
+    var cityValue = this.state.cityValue;
+    debugger;
+  },
+
   render: function() {
+    
+    var cityValue = this.state.cityValue;
+
     return (
       <div className="commentBox">
-        Concert App
+        <label>City Search:</label>
+        <input name="search-bar" type="text" onChange={this.handleChange} value={cityValue}></input>
+        <button onClick={this.handleClick}>Search</button>
       </div>
     );
   }
